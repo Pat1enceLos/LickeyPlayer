0b9127ebf1fe32209a2d16640ad8d4ea46ecadab
Tan Yang <1029862865@qq.com>
2019-04-01 23:39:54 +0800

    fix(tagedit): fix input blur event

--- src/renderer/components/mainContent/TagEdit.vue
+++ src/renderer/components/mainContent/TagEdit.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="tagEdit">
+  <div class="tagEdit" ref="tagsEdit">
     <div class="logoEdit">
       <img :src='picture' :style="{ width: '100%', height: '100%' }" v-show="picture">
     </div>
     <div class="titleEdit">
       <div class="titleTags">Title</div>
-      <input class="titleEditInput" v-model="title" @input="handleTitleInput" @blur="handleTitleBlur"/>
+      <input class="titleEditInput" v-model="title" @input="handleTitleInput"/>
       <Icon v-show="titleTags" type="save" :style="{ position: 'absolute', transform: 'translate(780%, 125%)', cursor: 'pointer' }" @mouseup.native="saveTitle"></Icon>
     </div>
     <div class="artistEdit">
       <div class="artistTags">Artist</div>
-      <input class="artistEditInput" v-model="artist" @input="handleArtistInput" @blur="handleArtistBlur"/>
+      <input class="artistEditInput" v-model="artist" @input="handleArtistInput"/>
       <Icon v-show="artistTags" type="save" :style="{ position: 'absolute', transform: 'translate(780%, 125%)', cursor: 'pointer' }" @mouseup.native="saveArtist"></Icon>
     </div>
     <div class="albumEdit">
       <div class="albumTags">Album</div>
-      <input class="albumEditInput" v-model="album" @input="handleAlbumInput" @blur="handleAlbumBlur"/>
+      <input class="albumEditInput" v-model="album" @input="handleAlbumInput"/>
       <Icon v-show="albumTags" type="save" :style="{ position: 'absolute', transform: 'translate(780%, 125%)', cursor: 'pointer' }" @mouseup.native="saveAlbum"></Icon>
     </div>
   </div>
@@ -35,6 +35,7 @@ export default {
       title: '暂无歌曲信息',
       artist: '暂无歌手信息',
       album: '暂无专辑信息',
+      inputClassList: ['titleEditInput', 'artistEditInput', 'albumEditInput'],
     };
   },
   components: {
@@ -53,36 +54,59 @@ export default {
       this.album = this.currentAudioInfo.album;
     },
   },
+  mounted() {
+    // this.$refs.tagsEdit.addEventListener('mousedown', (e) => {
+    //   if (['svg', 'use'].includes(e.target.nodeName)) {
+    //     e.preventDefault();
+    //   } else {
+    //     if (e.target.className !== 'titleEditInput') {
+    //       this.title = this.currentAudioInfo ? this.currentAudioInfo.title : '暂无歌曲信息';
+    //       this.titleTags = false;
+    //     }
+    //     if (e.target.className !== 'artistEditInput') {
+    //       this.artist = this.currentAudioInfo ? this.currentAudioInfo.artists : '暂无歌手信息';
+    //       this.artistTags = false;
+    //     }
+    //     if (e.target.className !== 'albumEditInput') {
+    //       this.album = this.currentAudioInfo ? this.currentAudioInfo.album : '暂无专辑信息';
+    //       this.albumTags = false;
+    //     }
+    //   }
+    // });
+  },
   methods: {
-    handleTitleBlur() {
-      this.title = this.currentAudioInfo.title;
-      this.titleTags = false;
-    },
-    handleArtistBlur() {
-      this.artist = this.currentAudioInfo.artists;
-      this.artistTags = false;
-    },
-    handleAlbumBlur() {
-      this.album = this.currentAudioInfo.album;
-      this.albumTags = false;
-    },
-    saveTitle() { // TODO save title with file
+    // handleTitleBlur() {
+    //   this.title = this.currentAudioInfo.title;
+    //   this.titleTags = false;
+    // },
+    // handleArtistBlur() {
+    //   this.artist = this.currentAudioInfo.artists;
+    //   this.artistTags = false;
+    // },
+    // handleAlbumBlur() {
+    //   this.album = this.currentAudioInfo.album;
+    //   this.albumTags = false;
+    // },
+    saveTitle() {
       const newVal = document.querySelector('.titleEditInput').value;
       const newCurrentAudioInfo = Object.assign({}, this.currentAudioInfo);
       newCurrentAudioInfo.title = newVal;
       this.$store.dispatch('updateCurrentAudioInfo', newCurrentAudioInfo);
+      this.titleTags = false;
     },
     saveArtist() {
       const newVal = document.querySelector('.artistEditInput').value;
       const newCurrentAudioInfo = Object.assign({}, this.currentAudioInfo);
       newCurrentAudioInfo.artists[0] = newVal;
       this.$store.dispatch('updateCurrentAudioInfo', newCurrentAudioInfo);
+      this.artistTags = false;
     },
     saveAlbum() {
       const newVal = document.querySelector('.albumEditInput').value;
       const newCurrentAudioInfo = Object.assign({}, this.currentAudioInfo);
       newCurrentAudioInfo.album = newVal;
       this.$store.dispatch('updateCurrentAudioInfo', newCurrentAudioInfo);
+      this.albumTags = false;
     },
     handleTitleInput() {
       this.titleTags = document.querySelector('.titleEditInput').value !== this.currentAudioInfo.title;
